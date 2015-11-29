da6ae7e44d4384d9042fc2c134af5a128dd8ec03
Cona19 <hyeonso5@gmail.com>
2015-11-29 20:19:35 +0900

    changed rw_lock_create_func to work concurrent

--- storage/innobase/sync/sync0rw.cc
+++ storage/innobase/sync/sync0rw.cc
@@ -267,14 +267,14 @@ rw_lock_create_func(
 	lock->event = os_event_create();
 	lock->wait_ex_event = os_event_create();
 
-	mutex_enter(&rw_lock_list_mutex);
+	//mutex_enter(&rw_lock_list_mutex);
 
-	ut_ad(UT_LIST_GET_FIRST(rw_lock_list) == NULL
-	      || UT_LIST_GET_FIRST(rw_lock_list)->magic_n == RW_LOCK_MAGIC_N);
+	ut_ad(UT_LIST_GET_FIRST_CONCUR(rw_lock_list) == NULL
+	      || UT_LIST_GET_FIRST_CONCUR(rw_lock_list)->magic_n == RW_LOCK_MAGIC_N);
 
-	UT_LIST_ADD_FIRST(list, rw_lock_list, lock);
+	UT_LIST_ADD_FIRST_CONCUR(list, rw_lock_list, lock);
 
-	mutex_exit(&rw_lock_list_mutex);
+	//mutex_exit(&rw_lock_list_mutex);
 }
 
 /******************************************************************//**
